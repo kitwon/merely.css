2948913106ff2d0558e141ddae98fbfc7ec0d1b5
Kitwang Chan <kitwang.c@gmail.com>
2020-02-25 16:32:23 +0800

    first commit

---
+++ .storybook/preview.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import { name, version } from '../package.json'
+import { configure, addParameters } from '@storybook/vue'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+import "../src/index.scss";
+
+addParameters({
+  options: {
+    brandTitle: `${name}@${version}`,
+    showAddonsPanel: false
+  },
+  viewport: {
+    viewports: {
+      sm: {
+        name: 'Small ($width-sm)',
+        styles: {width: '544px', height: 'auto'}
+      },
+      md: {
+        name: 'Medium ($width-md)',
+        styles: {width: '768px', height: 'auto'}
+      },
+      lg: {
+        name: 'Large ($width-lg)',
+        styles: {width: '1012px', height: 'auto'}
+      },
+      xl: {
+        name: 'XL ($width-xl)',
+        styles: {width: '1280px', height: 'auto'}
+      },
+      ...INITIAL_VIEWPORTS
+    }
+  }
+})
+
+configure(() => {
+  const loadMarkdown = require.context('../docs', true, /\.md$/)
+  for (const path of loadMarkdown.keys()) {
+    loadMarkdown(path)
+  }
+}, module)
---
+++ docs/utilities/border.md
@@ -0,0 +1,14 @@
+---
+title: Border
+---
+
+盒子工具类，包含Border、Border Radius 和 Box Shadow
+
+## Default Border
+默认border样式，1px soild $gray-200
+
+:::demo
+<template>
+<div class="border">.border</div>
+</template>
+:::
